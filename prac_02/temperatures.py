56b54fac3422ade2b6e996a65fc837660856b102
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-02-25 23:33:32 +0800

    Refactor temperatures.py program to use functions

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -2,16 +2,27 @@ MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
 
+
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return celsius * 9.0 / 5 + 32
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
+    return (fahrenheit - 32) * 5/9
+
+
 print(MENU)
 choice = input(">>> ").upper()
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        fahrenheit = celsius_to_fahrenheit(celsius)
         print(f"Result: {fahrenheit:.2f} F")
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = (fahrenheit - 32) * 5/9
+        celsius = fahrenheit_to_celsius(fahrenheit)
         print(f"Result: {celsius:.2f} C")
     else:
         print("Invalid option")
