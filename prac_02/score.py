d95325110dd49b8feec69a63e0beea8b4c3f9ea1
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-02-25 23:40:05 +0800

    Refactor score program

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,31 @@
+import random
+
 MINIMUM_SCORE = 0
 MAXIMUM_SCORE = 100
 EXCELLENT_SCORE = 90
 PASSABLE_SCORE = 50
 
-score = float(input("Enter score: "))
-if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
-    print("Invalid Score")
-elif score >= EXCELLENT_SCORE:
-    print("Excellent")
-elif score >= PASSABLE_SCORE:
-    print("Passable")
-else:
-    print("Bad")
+
+def determine_result(score):
+    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        return "Invalid Score"
+    elif score >= EXCELLENT_SCORE:
+        return "Excellent"
+    elif score >= PASSABLE_SCORE:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def main():
+    user_score = float(input("Enter score: "))
+
+    print(determine_result(user_score))
+
+    random_score = random.randint(MINIMUM_SCORE, MAXIMUM_SCORE)
+
+    print("Random score:", random_score)
+    print(determine_result(random_score))
+
+
+main()
