957ac7a1ccff9ecb063c311a7a98875812ffa50e
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-02-26 15:42:30 +0800

    Finished exceptions_demo.py program

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -9,10 +9,15 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
-    fraction = numerator / denominator
-    print(fraction)
+    if denominator == 0:
+        print("Cannot divide by zero!")
+    else:
+        fraction = numerator / denominator
+        print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
 print("Finished.")
+
+#1. ValueError will occur when the user enters a non-integer value when prompted for the numerator or denominator
+#2. ZeroDivisionError will occur if the user enters 0 as the denominator, leading to division by zero
+#3. Yes, by adding a condition to check if the denominator is zero before performing the division operation. If the denominator is zero, we can handle it, such as printing an error message
