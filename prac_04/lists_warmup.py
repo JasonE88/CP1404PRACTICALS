51c7814954d7479f982b489a5cef12508634b37e
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-04 13:48:42 +0800

    Finished list_warmup.py

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,19 +1,19 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-print(numbers[0])
-print(numbers[-1])
-print(numbers[3])
-print(numbers[:-1])
-print(numbers[3:4])
-print(5 in numbers)
-print(7 in numbers)
-print("3" in numbers)
-print(numbers + [6, 5, 3])
+print(numbers[0])          # Output: 3
+print(numbers[-1])         # Output: 2
+print(numbers[3])          # Output: 1
+print(numbers[:-1])        # Output: [3, 1, 4, 1, 5, 9]
+print(numbers[3:4])        # Output: [1]
+print(5 in numbers)        # Output: True
+print(7 in numbers)        # Output: False
+print("3" in numbers)      # Output: False
+print(numbers + [6, 5, 3]) # Output: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
-numbers[0] = "ten"
-numbers[-1] = 1
-print(numbers[2:])
-print(9 in numbers)
+numbers[0] = "ten"         # Change the first element to "ten"
+numbers[-1] = 1            # Change the last element to 1
+print(numbers[2:])         # Print all elements except the first two
+print(9 in numbers)        # Print whether 9 is an element of numbers
 
 
 
