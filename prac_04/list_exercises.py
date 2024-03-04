a0eddb31ef73f4592e99908938eec7da428b36b5
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-04 14:59:47 +0800

    Update list_exercises.py program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -13,3 +13,20 @@ def main():
 
 
 main()
+
+# Woefully inadequate security checker
+
+
+def main():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+    username_input = input("Enter your username: ")
+
+    if username_input in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+
+
+main()
