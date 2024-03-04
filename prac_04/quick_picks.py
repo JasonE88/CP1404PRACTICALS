f2f5d595acb78502b0fc50170d5a91bcddd0ed38
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-04 15:13:58 +0800

    Finished quick_picks.py program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,3 +4,22 @@ MIN_NUMBER = 1
 MAX_NUMBER = 45
 NUMBERS_PER_LINE = 6
 QUICK_PICKS_HEADER = "How many quick picks? "
+
+def main():
+    try:
+        num_quick_picks = int(input(QUICK_PICKS_HEADER))
+        quick_picks = [generate_quick_pick() for _ in range(num_quick_picks)]
+        display_quick_picks(quick_picks)
+    except ValueError:
+        print("Please enter a valid number.")
+
+
+def generate_quick_pick():
+    return sorted(random.sample(range(MIN_NUMBER, MAX_NUMBER + 1), NUMBERS_PER_LINE))
+
+def display_quick_picks(quick_picks):
+    for quick_pick in quick_picks:
+        print(" ".join(f"{number:2}" for number in quick_pick))
+
+
+main()
