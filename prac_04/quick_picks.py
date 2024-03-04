94b72eb0e3baa975908fcbdff817d967425e4021
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-04 15:51:01 +0800

    Update quick_picks.py program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,6 +6,7 @@ NUMBERS_PER_LINE = 6
 QUICK_PICKS_HEADER = "How many quick picks? "
 
 def main():
+    """Generate and display quick picks for the lottery."""
     try:
         num_quick_picks = int(input(QUICK_PICKS_HEADER))
         quick_picks = [generate_quick_pick() for _ in range(num_quick_picks)]
@@ -15,9 +16,12 @@ def main():
 
 
 def generate_quick_pick():
+    """Generate a quick pick consisting of random numbers."""
     return sorted(random.sample(range(MIN_NUMBER, MAX_NUMBER + 1), NUMBERS_PER_LINE))
 
+
 def display_quick_picks(quick_picks):
+    """Display the generated quick picks."""
     for quick_pick in quick_picks:
         print(" ".join(f"{number:2}" for number in quick_pick))
 
