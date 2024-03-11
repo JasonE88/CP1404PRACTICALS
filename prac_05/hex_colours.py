84a20755db91ea1bbff6e577ecbb4fe8a53a34c8
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-11 15:18:42 +0800

    Complete prac 5 hex colours exercise

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -10,3 +10,16 @@ COLOR_CODES = {
     "BLANCHEDALMOND": "#FFEBCD",
     "BLUE": "#0000FF"
 }
+
+print("Enter a color name to get its hexadecimal color code.")
+print("Enter a blank line to stop the program.")
+
+while True:
+    color_name = input("Enter a color name: ").upper()
+    if not color_name:
+        break
+    try:
+        color_code = COLOR_CODES[color_name]
+        print(f"The hexadecimal color code for {color_name} is {color_code}")
+    except KeyError:
+        print("Invalid color name. Please try again.")
