81c1edf3abb7a82ae9d61b4a1533026c05a29b83
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-11 15:05:55 +0800

    Complete prac 5 state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -14,6 +14,7 @@ CODE_TO_NAME = {
     "TAS": "Tasmania"
 }
 print(CODE_TO_NAME)
+
 max_length = max(len(code) for code in CODE_TO_NAME.keys())
 for code, name in CODE_TO_NAME.items():
     print(f"{code}{' ' * (max_length - len(code))} is {name}")
