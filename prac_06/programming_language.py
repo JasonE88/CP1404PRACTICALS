fd0e6c4f3a130dadbcb7a424989fcd3f5e971df1
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-18 14:50:18 +0800

    Start programming_language.py program

---
+++ prac_06/programming_language.py
@@ -0,0 +1,9 @@
+# Estimate: 30 minutes
+# Actual: 25 minutes
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
