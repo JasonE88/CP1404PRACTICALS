7a124ef0add4ade34c039bdfd74d220091287a3c
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-18 19:41:33 +0800

    Finished guitar.py program

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Guitar:
     """Represents a guitar with name, year, and cost attributes."""
 
@@ -24,3 +27,26 @@ class Guitar:
     def is_vintage(self):
         """Return True if the guitar is 50 or more years old, False otherwise."""
         return self.get_age() >= 50
+
+    def load_guitars(filename):
+        """Load guitars from a CSV file and return a list of Guitar objects."""
+        guitars = []
+        with open(filename, "r", newline="") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                name, year, cost = row
+                year = int(year)
+                cost = float(cost)
+                guitar = Guitar(name, year, cost)
+                guitars.append(guitar)
+        return guitars
+
+    def write_guitars(filename, guitars):
+        """Write guitars to a CSV file."""
+        with open(filename, "w", newline="") as file:
+            writer = csv.writer(file)
+            for guitar in guitars:
+                writer.writerow([guitar.name, guitar.year, guitar.cost])
+
+    load_guitars = staticmethod(load_guitars)
+    write_guitars = staticmethod(write_guitars)
