1529fdd6494c25b5e2d6ee532392161595739fe5
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-18 19:41:16 +0800

    Finished myguitars.py program

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -17,3 +17,26 @@ def main():
     # Write guitars to file
     Guitar.write_guitars(filename, guitars)
     print("Guitars written to file.")
+
+
+def display_guitars(guitars):
+    """Display guitars."""
+    print("Guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        print(f"{i}. {guitar}")
+
+
+def add_new_guitars(guitars):
+    """Add new guitars."""
+    while True:
+        name = input("Enter guitar name (blank to quit): ")
+        if not name:
+            break
+        year = int(input("Enter year made: "))
+        cost = float(input("Enter cost: "))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+
+
+if __name__ == "__main__":
+    main()
