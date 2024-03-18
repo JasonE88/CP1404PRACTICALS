eace37a8bd64486b4ec0816f39b1fc12c2ee4260
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-18 15:09:14 +0800

    Finished guitar.py program

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -12,3 +12,15 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        """Return a string representation of the Guitar object."""
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
+
+    def get_age(self):
+        """Return how old the guitar is in years."""
+        return 2024 - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is 50 or more years old, False otherwise."""
+        return self.get_age() >= 50
