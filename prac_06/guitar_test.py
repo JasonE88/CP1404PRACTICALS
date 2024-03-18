4c12387c755ddd583627b82fd546855979efd277
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-18 15:14:35 +0800

    Finished guitar_test.py program

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -6,3 +6,11 @@ def run_tests():
     guitar1 = Guitar("Gibson L-5 CES", 1922, 16035.40)
     print(f"{guitar1.name} get_age() - Expected 100. Got {guitar1.get_age()}")
     print(f"{guitar1.name} is_vintage() - Expected True. Got {guitar1.is_vintage()}")
+
+    # Test Guitar 2
+    guitar2 = Guitar("Another Guitar", 2013, 1000)
+    print(f"{guitar2.name} get_age() - Expected 11. Got {guitar2.get_age()}")
+    print(f"{guitar2.name} is_vintage() - Expected False. Got {guitar2.is_vintage()}")
+
+
+run_tests()
