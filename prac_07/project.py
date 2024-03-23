597ba8f2786428cc9191f4f8ae6e6430464f2b03
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-23 16:34:00 +0800

    Finished project.py program

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,27 +1,28 @@
-# Estimate: 60 minutes
-# Actual:  minutes
-
 import datetime
 
 class Project:
-    """Represents a project with name, start date, priority, estimate, and completion attributes."""
+    """Represents a project with attributes such as name, start date, priority, estimate, and completion."""
 
     def __init__(self, name, start_date, priority, estimate, completion):
-        """Initialize a Project instance."""
+        """Initialize a Project object with given attributes."""
         self.name = name
-        self.start_date = start_date
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = priority
         self.estimate = estimate
         self.completion = completion
 
-    # Implement methods to load and save projects from/to a data file
+    def __str__(self):
+        """Return a string representation of the project."""
+        return f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, estimate: ${self.estimate:.2f}, completion: {self.completion}%"
 
-    def load_projects(filename):
-        """Load projects from a data file."""
-        # Implement loading projects from the data file
-        pass
+    def update_completion(self, new_completion):
+        """Update the completion percentage of the project."""
+        self.completion = new_completion
 
-    def save_projects(filename, projects):
-        """Save projects to a data file."""
-        # Implement saving projects to the data file
-        pass
+    def update_priority(self, new_priority):
+        """Update the priority of the project."""
+        self.priority = new_priority
+
+    def get_priority(self):
+        """Get the priority of the project."""
+        return self.priority
