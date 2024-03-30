98ed5784b04f243d56863732627de921918c3036
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-03-30 16:07:30 +0800

    Updated dynamic_labels.py

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -7,4 +7,13 @@ class DynamicLabelsApp(App):
     names = ["Jason", "Ralph", "Charlie", "Dexter", "Wilson"]
 
     def build(self):
+        self.title = "Dynamic Labels"
+        layout = BoxLayout(orientation='vertical')
+        for name in self.names:
+            label = Label(text=name, font_size='24sp', color=(0, 0, 1, 1))
+            layout.add_widget(label)
+        return layout
 
+
+if __name__ == "__main__":
+    DynamicLabelsApp().run()
