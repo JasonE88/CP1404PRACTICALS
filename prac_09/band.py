a18d3c82f186fd68456f64ce5b692c88ecb74fdc
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-04-14 18:17:10 +0800

    Finished band.py program

--- prac_09/band.py
+++ prac_09/band.py
@@ -5,3 +5,19 @@ class Band:
         """Initialize a Band with a name and an empty list of musicians."""
         self.name = name
         self.musicians = []
+
+    def __str__(self):
+        """Return a string representation of a Band."""
+        musician_info = ", ".join(str(musician) for musician in self.musicians)
+        return f"{self.name} ({musician_info})"
+
+    def add(self, musician):
+        """Add a musician to the band."""
+        self.musicians.append(musician)
+
+    def play(self):
+        """Simulate the band playing, returning a list of strings indicating who is playing what."""
+        play_results = []
+        for musician in self.musicians:
+            play_results.append(musician.play())
+        return play_results
