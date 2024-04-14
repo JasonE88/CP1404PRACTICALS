d0d4ac5e0321f28730b929d18e6960dd8ed320c6
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-04-14 15:51:54 +0800

    Add taxi_test.py program

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,18 @@
+from prac_09.taxi import Taxi
+
+
+def main():
+    """Test the Taxi class."""
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+
+    my_taxi.drive(40)
+
+    print(f"Taxi details:\n{my_taxi}\nCurrent fare: ${my_taxi.get_fare():.2f}\n")
+
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+
+    print(f"Taxi details:\n{my_taxi}\nCurrent fare: ${my_taxi.get_fare():.2f}\n")
+
+
+main()
