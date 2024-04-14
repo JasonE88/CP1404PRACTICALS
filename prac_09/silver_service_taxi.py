d7a4d607dbaf437675903558eae6e29bd7b20acb
jasonelmer <jasonelmer.borisman@my.jcu.edu.au>
2024-04-14 17:20:55 +0800

    Update silver_service_taxi.py program

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -5,15 +5,16 @@ class SilverServiceTaxi(Taxi):
     """Represent a SilverServiceTaxi."""
     flagfall = 4.50
 
-    def __init__(self, name, fuel, price_per_km, fanciness):
-        """Initialise a SilverServiceTaxi instance, based on parent class Taxi."""
-        super().__init__(name, fuel, price_per_km)
+    def __init__(self, name, fuel, fanciness):
+        """Initialise a SilverServiceTaxi."""
+        super().__init__(name, fuel)
         self.fanciness = fanciness
+        self.price_per_km *= fanciness
 
     def __str__(self):
-        """Return a string like a Taxi but with flagfall."""
+        """Return a string representation of a SilverServiceTaxi."""
         return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
 
     def get_fare(self):
-        """Return the price for the taxi trip."""
-        return super().get_fare() + self.flagfall
+        """Get the current fare."""
+        return self.flagfall + super().get_fare()
